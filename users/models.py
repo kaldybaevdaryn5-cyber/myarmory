d55cd4043ea30e14a08748e1677350df64d63d6f
Medet <medet@example.com>
2025-10-16 12:40:36 +0500

    Add secret command to create superuser

--- users/models.py
+++ users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics', verbose_name="Аватар")
+    # ▼▼▼ ИЗМЕНИ ЭТУ СТРОЧКУ, ДОБАВИВ blank=True ▼▼▼
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics', verbose_name="Аватар", blank=True)
 
     def __str__(self):
         return f'Профиль пользователя {self.user.username}'
